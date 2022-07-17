071a209643c9cd4948faffbd085b4e301d9d3e78
Raman Srinivasa Venkat <ramansv710@gmail.com>
2022-07-17 23:12:46 +0530

    catalog api microservice.

---
+++ src/Services/Catalog/Catalog.API/Repositories/IProductRepository.cs
@@ -0,0 +1,15 @@
+﻿using Catalog.API.Entities;
+
+namespace Catalog.API.Repositories
+{
+    public interface IProductRepository
+    {
+        Task CreateProduct(Product product);
+        Task<bool> DeleteProduct(string id);
+        Task<Product> GetProduct(string id); 
+        Task<IEnumerable<Product>> GetProducts();        
+        Task<IEnumerable<Product>> GetProductByCategory(string categoryName);
+        Task<IEnumerable<Product>> GetProductByName(string name);                
+        Task<bool> UpdateProduct(Product product);
+    }
+}
